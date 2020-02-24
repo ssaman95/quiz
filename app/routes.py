6fa423bf2ce995716ac2191bd1ad755354a3eb77
Aman <aman199596@gmail.com>
2020-02-25 02:39:10 +0530

    Created first quiz question without submit options

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
-from app import app
+from app import app, mongo
+from flask import render_template, jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World! to the Quiz app"
+    ques_list = mongo.db.quiz.find()
+    # print('ques_list: ' + jsonify(ques_list))
+    # print(render_template('index.html', ques_list=ques_list))
+    return render_template('index.html', ques_list=ques_list)
+    # return "Hello, World! to the Quiz app"
--- quiz.py
+++ quiz.py
@@ -1,1 +1,5 @@
-from app import app
+from app import app, mongo
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'mongo': mongo}
