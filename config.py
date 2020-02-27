28119ef3d906dd746bb2a60a7437b73f4629404c
Aman <aman199596@gmail.com>
2020-02-28 02:28:18 +0530

    added user model

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '4cyjhdf-fusdf84i-usao9di'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
