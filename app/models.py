1905c35798292d8643710bb7a4f06e1b0e50617d
Aman <aman199596@gmail.com>
2020-03-02 22:39:39 +0530

    Added registration form

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(64), nullable=False)
     lname = db.Column(db.String(64), nullable=False)
@@ -18,3 +19,8 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
